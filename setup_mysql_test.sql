b058074ea1af5abcd707c8bf23c9325f586f8c98
OrnellaNiyibizi <o.mahoro@alustudent.com>
2023-06-05 14:18:31 +0200

    commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,9 @@
--- This Prepares a MYSQL server for the project
+-- Creates the database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- add the user hbnb_test identified with password 'hbnb_test_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- add privileges to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- refresh to apply privileges
+-- FLUSH PRIVILEGES;
